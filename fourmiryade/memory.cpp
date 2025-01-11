6924d1d667667bd941f5c20e330e48a55d08758a
MDLC01 <57839069+MDLC01@users.noreply.github.com>
2025-01-11 15:53:56 +0100

    Reorder functions

--- fourmiryade/memory.cpp
+++ fourmiryade/memory.cpp
@@ -27,70 +27,10 @@ int ceil_log2(unsigned long long x)
   return y;
 }
 
-#define BYTE_SIZE 8
-
-int read_bit(unsigned char n, int offset) {
-    assert(0 <= offset && offset < BYTE_SIZE);
-    return (n << offset) >> BYTE_SIZE;
-}
-
-unsigned char write_bit(unsigned char n, int offset, unsigned int value) {
-    assert(0 <= offset && offset < BYTE_SIZE);
-    assert(value == 0 || value == 1);
-    unsigned char masks[8] = {
-        0b10000000,
-        0b01000000,
-        0b00100000,
-        0b00010000,
-        0b00001000,
-        0b00000100,
-        0b00000010,
-        0b00000001,
-    };
-    n = n & ~masks[offset];
-    return n | (value << (BYTE_SIZE - offset + 1));
-}
-
-unsigned int read_int(unsigned char *mem, int bit_offset, unsigned int size) {
-    assert(0 <= bit_offset);
-    if (size == 0) {
-        return 0;
-    }
-    while (bit_offset >= BYTE_SIZE) {
-        mem = mem + 1;
-        bit_offset = bit_offset - BYTE_SIZE;
-    }
-    int n = read_int(mem, bit_offset + 1, size - 1);
-    return (n << 1) + read_bit(*mem, bit_offset);
-}
-
-void write_int(unsigned char *mem, int bit_offset, int size, unsigned int value) {
-    assert(0 <= bit_offset);
-    if (size == 0) {
-        return;
-    }
-    while (bit_offset >= sizeof(char)) {
-        mem = mem + 1;
-        bit_offset = bit_offset - sizeof(char);
-    }
-    *mem = write_bit(*mem, bit_offset, value % 2);
-    write_int(mem, bit_offset + 1, size - 1, value >> 1);
-}
-
-
-typedef enum {
-    EXPLORATRICE,
-    CARTOGRAPHE,
-    OUVRIERE,
-    VARIANT_COUNT,
-} type_fourmi;
-
-type_fourmi get_type(unsigned char *mem) {
-    int size = ceil_log2(VARIANT_COUNT);
-    return (type_fourmi) read_int(mem, 0, ceil_log2(VARIANT_COUNT));
-}
-
-
+typedef struct {
+    unsigned char *mem;
+    int offset;
+} reader;
 
 char get_bit(char* mem, int x) {
     int i = x/8;
@@ -133,4 +73,17 @@ void set_number (char* mem, int min, int max, unsigned long long number) {
         set_bit(mem, x, bit);
         number = number >> 1;
     }
+}
+
+
+typedef enum {
+    EXPLORATRICE,
+    CARTOGRAPHE,
+    OUVRIERE,
+    VARIANT_COUNT,
+} type_fourmi;
+
+type_fourmi get_type(unsigned char *mem) {
+    int size = ceil_log2(VARIANT_COUNT);
+    return (type_fourmi) read_int(mem, 0, ceil_log2(VARIANT_COUNT));
 }
