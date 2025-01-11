5408b18c8ae545006781d91704cea48175037322
Armand Ledoux <armand.ledoux@ens-rennes.fr>
2025-01-11 18:24:51 +0100

    add queen_main

--- fourmiryade/queen.cpp
+++ fourmiryade/queen.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
+#include <cstdint>
 #include <map>
+#include "sinfourmis.h"
+
 using namespace std;
 
 #define MAX_NODES 1000
@@ -13,6 +16,51 @@ typedef struct {
     int node;
     path path;
 } path_to_node_map;
+
+#define min(a,b) (a)<(b)?(a):(b)
+
+enum queen_turn_t {RECUP_TURN, RELEASE_TURN, BIRTH_TURN};
+queen_turn_t turn_type = RECUP_TURN;
+long turn_count = 0;
+
+
+
+
+void read_memory (fourmi_etat) {} 
+
+void set_memory(); //TODO
+
+reine_retour queen_main(fourmi_etat fourmis[], const unsigned int nb_fourmis, const reine_etat *etat, const salle *salle) {
+  for (unsigned int i = 0; i<nb_fourmis; i++) {
+    read_memory(fourmis[i]);
+  }
+  if (turn_count == 0) {
+    turn_count +=1;
+    return {.action = CREER_FOURMI, .arg = (int32_t)etat->nourriture/4};
+  }
+  else if (turn_count == 1) {
+    turn_count +=1;
+    for (unsigned int i=0; i<nb_fourmis; i++) {
+      set_memory();
+    }
+  }
+  else switch (turn_type) {
+    case RECUP_TURN: //TODO
+      turn_type = BIRTH_TURN;
+      return {.action = RECUPERER_FOURMI, .arg=0};
+
+
+    case BIRTH_TURN : //TODO
+      turn_type = RELEASE_TURN;
+      return {.action = CREER_FOURMI, .arg=0};
+
+    case RELEASE_TURN: //TODO
+      for (unsigned int i=0; i<nb_fourmis; i++) {
+        set_memory();
+      }
+      return {.action = ENVOYER_FOURMI, .arg=0};
+      turn_type = RECUP_TURN;
+}
 
 static map<path, node> known_paths;
 // `adj[u][v]` indicates what choice to make to go from `u` to `v`.
